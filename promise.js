34ff8e5db6a50bef1f38aa060bc94b0b2a4beca2
fpozzobon <fabien.pozzobon@gmail.com>
2019-02-02 12:08:34 +0200

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,30 +1,33 @@
-const res = (value) => value
-const rej = (value) => value
-
 const PromiseTest = (fn) => {
   let res = {}
   fn((value) => {
     res['value'] = value;
+  }, (err) => {
+    res['error'] = err;
   })
   
-  return {getValue: () => res['value']}
+  return {getValue: () => res['value'], getError: () => res['error']}
 }
 
 
-const defer = (res) => (callback) => (p) => {
+const defer = (res) => (rej) => (callback) => (error) => (p) => {
   if (p.getValue()) {
     res(callback(p.getValue()))
+  } else if (p.getError()) {
+      error ? rej(error(p.getError())): rej(p.getError());
   } else {
-    setTimeout(() => defer(res)(callback)(p), 100);
+    setTimeout(() => defer(res)(rej)(callback)(error)(p), 100);
   }
 }
 
-const then = (p) => (callback) => {
+const then = (p) => (callback, error) => {
   return PromiseTest((res, rej) => {
     if (p.getValue()) {
-        res(callback(p.getValue()))
+      res(callback(p.getValue()))
+    } else if (p.getError()) {
+      error ? rej(error(p.getError())): rej(p.getError());
     } else {
-        setTimeout(() => defer(res)(callback)(p), 100);
+        setTimeout(() => defer(res)(rej)(callback)(error)(p), 100);
     }
     
   })
@@ -33,11 +36,11 @@ const then = (p) => (callback) => {
                                 
 const data = PromiseTest((res, rej) => {
   console.log('execute')
-  // res(1)
+  res(1)
   setTimeout(() => res(1), 100);
     });
 
 then(data)(e => console.log(e))
 then(data)(e => console.log(e))
 
-then(then(data)(e => e * 4))(e => console.log(e))
+then(then(data)(e => e * 4, e => "test"))(e => console.log(e), e => console.log("Error " + e))
